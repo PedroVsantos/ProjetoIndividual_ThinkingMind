a299a97273590bab201b14c65ec75b68a668729f
PedroVsantos <pedro.vsantos@sptech.school>
2024-06-04 02:50:54 -0300

    site V7 finalizado

---
+++ site V7 - final/src/models/medidaModel.js
@@ -0,0 +1,31 @@
+var database = require("../database/config");
+
+function buscarUltimasMedidas(idUser) {
+
+    var instrucaoSql = `
+    select pontuacao, data_hora as dtHora from quiz
+    join usuario on usuario_id = usuario.id
+    where usuario_id = ${idUser};`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTempoReal(id) {
+
+    var instrucaoSql = `SELECT 
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,
+                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+                        fk_aquario 
+                        FROM medida WHERE fk_aquario = ${idAquario} 
+                    ORDER BY id DESC LIMIT 1`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    buscarUltimasMedidas,
+    buscarMedidasEmTempoReal
+}
